88e7092997f89391bac77e53034e0559704cf7fe
Anmol Sinha <anmolsinha1201@outlook.com>
2021-06-07 06:27:41 +0300

    Added navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,7 +1,29 @@
+import styles from './Navbar.module.css'
+import {useState} from 'react'
+
 const Navbar = () => {
+	const NavMenuItems = ['Home', 'Contact Us']
+	const [selectedIndex, setSelectedIndex] = useState(0);
+
 	return (
-		<div>
-			Hi
+		<div className={styles.NavbarWrapper}>
+			<div className={styles.Title}>
+				Schedule Helper
+			</div>
+			<div className={styles.NavigationItems}>
+				{
+					NavMenuItems.map((item, index) => (
+						<div  key={index}
+						className={[styles.SingleNavigationItem, selectedIndex == index ? styles.SelectedItem : null].filter(i => i!= null).join(' ')} 
+						onClick={() => setSelectedIndex(index)} >
+							<div className={styles.TextContainer}>
+								{item}
+							</div>
+						</div>
+					))
+				}
+				
+			</div>
 		</div>
 	)
 }
